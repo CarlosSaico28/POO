b3e6591067c5e1ea8c3a1fb7dccf1a7dd43c8896
CarlosSaico28 <carlos.saico.mmc@gmail.com>
2021-08-02 08:18:37 -0500

    Primer archivo git

--- ExamenFinalPOO/ExamenFinal/src/Modelo/Producto.java
+++ ExamenFinalPOO/ExamenFinal/src/Modelo/Producto.java
@@ -49,7 +49,7 @@ public class Producto {
         this.comprador = comprador;
         this.listaFactura=new ArrayList();
     }
-xxxxxxxxxxxxxxxxxxxxxx
+
     public Producto(long generarId, String descripcion, double precioUnitario, int stock, double iva, Cliente comprador, Cliente cliente, Factura factura) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
