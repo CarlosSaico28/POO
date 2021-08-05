d621b425179ea83464de1a38538a0fb4e5653104
CarlosSaico28 <carlos.saico.mmc@gmail.com>
2021-08-04 20:42:58 -0500

    Primer archivo guit

---
+++ IntroduccionGUI/src/introducciongui/IntroduccionGUI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introducciongui;
+
+import eventos.VentanaRaton;
+import graficas.Ventana;
+import seleccionFigura.VentanaFigura;
+import vista.VistaInicio;
+
+/**
+ *
+ * @author JOSE
+ */
+public class IntroduccionGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        
+        VentanaFigura ventanaFigura = new VentanaFigura();
+        ventanaFigura.setVisible(true);
+    }
+    
+}
